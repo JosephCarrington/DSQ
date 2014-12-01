922aa30f263eed648dac4fcca902f527d03e5913
Joseph Carrington <joseph.carrington@gmail.com>
2014-11-30 20:21:40 -0600

    Getting rid of default mailchimp styles

--- themes/dsq/mailchimp.php
+++ themes/dsq/mailchimp.php
@@ -6,7 +6,7 @@
 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_05cc8f7aee6a36a5f081f197a_7e048eecb3" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
     </div>
 </form>
 </div>
