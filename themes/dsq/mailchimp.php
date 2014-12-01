67aa81dbc8118a4a0e379da3eed0f395c964910a
Joseph Carrington <joseph.carrington@gmail.com>
2014-11-30 18:41:04 -0600

    Mailchimp File

---
+++ themes/dsq/mailchimp.php
@@ -0,0 +1,15 @@
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<div id="mc_embed_signup">
+<form action="//datasociety.us9.list-manage.com/subscribe/post?u=05cc8f7aee6a36a5f081f197a&amp;id=7e048eecb3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_05cc8f7aee6a36a5f081f197a_7e048eecb3" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
