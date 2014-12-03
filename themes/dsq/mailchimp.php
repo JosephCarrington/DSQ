f3995e2c0ca1e6f952850ef25bc8957a567cf38b
Joseph Carrington <joseph.carrington@gmail.com>
2014-12-03 13:54:27 -0600

    header styles, again

--- themes/dsq/mailchimp.php
+++ themes/dsq/mailchimp.php
@@ -2,7 +2,7 @@
 <div id="mc_embed_signup">
 <form action="//datasociety.us9.list-manage.com/subscribe/post?u=05cc8f7aee6a36a5f081f197a&amp;id=7e048eecb3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Subscribe to our mailing list</label>
+	<label for="mce-EMAIL">subscribe to our newsletter</label>
 	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_05cc8f7aee6a36a5f081f197a_7e048eecb3" tabindex="-1" value=""></div>
