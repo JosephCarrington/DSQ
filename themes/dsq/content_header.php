61f310acd06bc83645a3dd212c4d683f93549e53
Joseph Carrington <joseph.carrington@gmail.com>
2014-12-03 13:56:07 -0600

    header styles, again

--- themes/dsq/content_header.php
+++ themes/dsq/content_header.php
@@ -29,7 +29,7 @@
 			}
 		}
 	</style>
-<header class='content-header-wrapper header-<?php the_title_attribute(); ?>' id='header-<?php the_ID(); ?>'>
+<header class='content-header-wrapper header-<?php echo $post->post_name; ?>' id='header-<?php the_ID(); ?>'>
 	<hgroup class='content-header' style='background-color: rgba(<?php echo $rgb_bg_color[0] . ',' . $rgb_bg_color[1] . ',' . $rgb_bg_color[2]; ?>, 0.75);' >
 		<?php if(get_field('sub-subtitle')) echo "<h3>" . get_field('sub-subtitle') . "</h3>"; ?>
 		<h1><?php the_title(); ?></h1>
